df09f820c6aaff118b085f90a561223fda747719
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-11-06 22:06:45 +0900

    스프링 가이드 - 배치 예제 따라하기

---
+++ batch/src/main/java/com/guide/batch/BatchConfiguration.java
@@ -0,0 +1,63 @@
+package com.guide.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BatchConfiguration {
+    @Bean
+    public FlatFileItemReader<Person> reader() {
+        return new FlatFileItemReaderBuilder<Person>()
+                .name("personItemReader")
+                .resource(new ClassPathResource("sample-data.csv"))
+                .delimited()
+                .names("firstName", "lastName")
+                .targetType(Person.class)
+                .build();
+    }
+
+    @Bean
+    public PersonItemProcessor processor() {
+        return new PersonItemProcessor();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Person> writer(DataSource dataSource) {
+        return new JdbcBatchItemWriterBuilder<Person>()
+                .sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)") // db에 데이터 저장
+                .dataSource(dataSource)
+                .beanMapped()
+                .build();
+    }
+    @Bean
+    public Job importUserJob(JobRepository jobRepository, Step step1, JobCompletionNotificationListener listener) {
+        return new JobBuilder("importUserJob", jobRepository)
+                .listener(listener)
+                .start(step1)
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
+                      FlatFileItemReader<Person> reader, PersonItemProcessor processor, JdbcBatchItemWriter<Person> writer) {
+        return new StepBuilder("step1", jobRepository)
+                .<Person, Person> chunk(3, transactionManager) // 3개의 데이터를 묶어서 처리
+                .reader(reader) // csv 파일에서 데이터를 읽어옴
+                .processor(processor) // 데이터를 가공 (firstName, lastName을 대문자로 변경)
+                .writer(writer) // 데이터를 db에 저장
+                .build();
+    }
+}
---
+++ batch/src/main/java/com/guide/batch/Person.java
@@ -0,0 +1,6 @@
+package com.guide.batch;
+
+public record Person(String firstName, String lastName) {
+
+}
+
